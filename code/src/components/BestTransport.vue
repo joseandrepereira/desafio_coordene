3659de264afa770b2ced789b121d2e7de20a1773
joseandre1706 <jose.andre.psn>
2022-07-16 20:56:03 -0300

    componente de Form criado

--- code/src/components/BestTransport.vue
+++ code/src/components/BestTransport.vue
@@ -1,52 +1,94 @@
 <template>
-  <div class="title">
-    <b-navbar toggleable="lg" type="dark" variant="info">
-      <b-navbar-brand class="ml-2">
-        <b>{{ appName }}</b>
-      </b-navbar-brand>
-    </b-navbar>
-  </div>
+  <div>
+    <div class="title">
+      <b-navbar toggleable="lg" type="dark" variant="info">
+        <b-navbar-brand class="ml-2">
+          <img src="../assets/logo.png" alt="logo.png" height="45" width="45" />
+          <b>{{ appName }}</b>
+        </b-navbar-brand>
+      </b-navbar>
+    </div>
 
+    <div class="body">
+      <TransportForm :shippingData="shippingData" />
+      <p>\\\\\\\\\\ RESULTADOS ////////////</p>
+    </div>
+  </div>
 </template>
 
 <script>
-import {
-  BNavbar,
-  BNavbarBrand,
-} from 'bootstrap-vue'
+import { BNavbar, BNavbarBrand } from "bootstrap-vue";
+import TransportForm from "./TransportForm.vue";
 
 export default {
+  name: "BestTransport",
+
   components: {
     BNavbar,
     BNavbarBrand,
+    TransportForm,
   },
   data() {
-    const appName = ''
+    const appName = "";
+    const shippingData = null;
 
     return {
       appName,
-    }
+      shippingData,
+    };
   },
   created() {
     // Implemente aqui o GET dos dados da API REST
     // para que isso ocorra na inicialização da pagina
-    this.appName = 'Melhor Frete'
+    this.appName = "MELHOR FRETE";
   },
   methods: {
     // Implemente aqui os metodos utilizados na pagina
     methodFoo() {
-      console.log(this.appName)
+      console.log(this.appName);
     },
+    async getDatas(){
+      const req = await fetch("http://localhost:3000/transport");
+      const data = await req.json();
+
+      this.shippingData = data;
+
+      // console.log(this.shippingData);
+    }
   },
-}
+  mounted(){
+    this.getDatas()
+  }
+};
 </script>
 
 <style scoped>
 .title .navbar {
-  background-color: #00aca6 !important;
+  background-color: #93c47d !important;
+  border-bottom: 2px solid #000;
+  padding: 15px 30px;
 }
 
 .title .navbar-brand {
+  background-color: rgba(0, 0, 0, 0.519);
+
   margin-left: 20px;
+
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+}
+
+.title .navbar-brand b {
+  margin-left: 15px;
+  color: black;
+  font-size: larger;
+}
+
+.body {
+  display: flex;
+  justify-content:center;
+  align-items: center;
+  flex-direction: column;
 }
 </style>
---
+++ code/src/components/TransportForm.vue
@@ -0,0 +1,88 @@
+<template>
+  <div>
+    <p><strong>Como é o frete que você precisa?</strong></p>
+    <div>
+      <form id="transport-form">
+        <div class="input-container">
+          <label for="destino">Destino</label>
+          <select name="destino" id="destino" v-model="destino">
+            <option value="">Selecione aqui o destino do frete</option>
+            <option  value="">Selecione aqui o destino do frete</option>
+          </select>
+        </div>
+        <div class="input-container">
+          <label for="peso">Peso</label>
+          <input type="text" id="peso" name="name" v-model="peso" placeholder="Insira aqui o peso da carga em Kg" />
+        </div>
+        <div class="input-container">
+            <input type="submit" class="submit-btn" value="Analisar">
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TransportForm",
+  props: ['shippingData'],
+  data(){
+    return {
+        destinations: [],
+    }
+  },
+  mounted(){
+  //  this.destinations = this.shippingData;
+   // console.log(this.shippingData);
+  }
+};
+</script>
+
+<style scoped>
+
+    p {
+    padding: 0 15px;
+    border-bottom: 2px solid #000;
+    width: 300px;
+    }
+
+    #transport-form {
+        max-width: 400px;
+        margin: 0 auto;
+    }
+
+    .input-container {
+        display: flex;
+        flex-direction: column;
+        margin-bottom: 20px;
+        padding: 0 20px;
+
+    }
+
+    .submit-btn {
+       background-color: #93c47d;
+       border-radius: 10px;
+       width: 130px;
+       font-weight: bold;
+       padding: 5px 10px;
+       margin: 0 auto;
+       cursor: pointer;
+       transition: .5s;
+    }
+
+    .submit-btn:hover{
+        background-color: #244517;
+        color: #fafafa;
+        border-color: #000000;
+    }
+
+    label {
+        font-weight:400;
+    }
+
+    input, select {
+        padding: 5px 10px;
+        width: 350px;
+    }
+
+</style>
